53bfcbbfe7207e9cc79965ff8068fb91939bc9dd
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-29 23:19:28 +0330

    init: js

---
+++ script.js
@@ -0,0 +1,8 @@
+// eventlistener for when internet connection is lost
+window.addEventListener('offline', function(e) {
+    // do something
+});
+// eventlistener for when internet connection is restored
+window.addEventListener('online', function(e) {
+    // do something
+});
